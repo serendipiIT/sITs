53f373293473176e5551372c96f6aa4da004b719
Johan Hermansson <karljohan.hermanssons@gmail.com>
2023-02-17 14:48:09 +0100

    adds carousel on hompage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script>
-  import CitiesList from './components/CitiesList.vue'
-  import HelloWorld from './components/HelloWorld.vue'
+  import CitiesList from "./components/CitiesList.vue"
+  import HelloWorld from "./components/HelloWorld.vue"
+  import Carousel from "./components/Carousel.vue"
 
   export default {
     components: {
       CitiesList,
-      HelloWorld
-    }
+      HelloWorld,
+      Carousel,
+    },
   }
 </script>
 
 <template>
   <HelloWorld msg="Hello World!" />
+  <Carousel class="my-10" />
   <CitiesList />
 </template>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,49 @@
+<script>
+  import { Swiper, SwiperSlide } from "swiper/vue"
+  import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper"
+
+  import "swiper/css"
+  import "swiper/css/navigation"
+  import "swiper/css/pagination"
+  import "swiper/css/scrollbar"
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      const onSwiper = (swiper) => {
+        console.log(swiper)
+      }
+      const onSlideChange = () => {
+        console.log("slide change")
+      }
+      return {
+        onSwiper,
+        onSlideChange,
+        modules: [Navigation, Pagination, A11y, Autoplay],
+      }
+    },
+  }
+</script>
+
+<template>
+  <swiper
+    :modules="modules"
+    :slides-per-view="3"
+    :space-between="50"
+    navigation
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+    class="text-center"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
+    ...
+  </swiper>
+</template>
