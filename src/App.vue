3d6ef6d1fe036b3bdd8646726bde8e9d43cddfa8
Johan Hermansson <karljohan.hermanssons@gmail.com>
2023-02-20 15:17:36 +0100

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <script>
-  import CitiesList from "./components/CitiesList.vue"
-  import HelloWorld from "./components/HelloWorld.vue"
-  import Carousel from "./components/Carousel.vue"
-
   export default {
     created() {
       this.$store.dispatch("get")
-    },
-    components: {
-      CitiesList,
-      HelloWorld,
-      Carousel,
     },
   }
 </script>
