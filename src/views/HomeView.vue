f75f8541569654b8fbf250ab95622df076736133
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-02-23 11:13:05 +0100

    add discover section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script>
   import { mapGetters } from "vuex"
   import Carousel from "../components/Carousel.vue"
+  import DiscoverSection from "../components/DiscoverSection.vue"
   import NewProducts from "../components/NewProducts.vue"
   import Hero from "../components/HeroSection.vue"
 
   export default {
     components: {
       Carousel,
+      DiscoverSection,
       NewProducts,
       Hero,
     },
@@ -31,6 +33,7 @@
       :product-list="categories"
       :breakpoints="{ 1024: { slidesPerView: 4 }, 768: { slidesPerView: 2 } }"
     />
+    <DiscoverSection />
     <NewProducts />
   </main>
 </template>
