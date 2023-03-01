02f05accf88cdf50fc6d224ac8e6981692e0b137
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-03-01 10:10:38 +0100

    uses productcard for newproducts on homepage

--- src/components/NewProducts.vue
+++ src/components/NewProducts.vue
@@ -2,10 +2,10 @@
   <h2 class="text-3xl text-center mb-12">New products</h2>
   <Carousel
     class="max-w-[95vw]"
-    :height="'h-[60vh]'"
     :info-text="true"
     :product-list="productList"
-    :slides-per-view="2"
+    :slides-per-view="1"
+    :breakpoints="{ 1024: { slidesPerView: 3 }, 768: { slidesPerView: 2 } }"
   />
 </template>
 
