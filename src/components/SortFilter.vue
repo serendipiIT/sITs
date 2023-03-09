bb7cdb875b9b9eaa2205e64a07cddb238aeaab9a
acervall <ameliecervall@gmail.com>
2023-03-09 09:40:13 +0100

    filter kaos

--- src/components/SortFilter.vue
+++ src/components/SortFilter.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex justify-between p-2 mt-4">
-    <button class="btn btn-invert" @click="isOpen = !isOpen" v-show="!isOpen">
-      Filter
-    </button>
+    <button class="btn btn-invert" @click="isOpen = !isOpen">Filter</button>
     <button class="btn btn-invert">Sort By</button>
   </div>
 
@@ -16,23 +14,60 @@
       class="select-none md:flex-col absolute top-16 left-0 h-screen bg-neutral-50 px-10"
       v-if="isOpen"
     >
-      <routerLink
-        :to="{ name: 'home' }"
-        class="block mb-2 font-semibold text-gray-800 py-2 px-3 hover:bg-gray-200 rounded cursor-pointer"
-        >Home</routerLink
-      >
-
-      <routerLink
-        :to="{ name: 'products' }"
-        class="block mb-2 font-semibold text-gray-800 py-2 px-3 hover:bg-gray-200 rounded cursor-pointer"
-        >Products</routerLink
-      >
-
-      <routerLink
-        :to="{ name: 'about' }"
-        class="block mb-2 font-semibold text-gray-800 py-2 px-3 hover:bg-gray-200 rounded cursor-pointer"
-        >About</routerLink
-      >
+      <div class="flex flex-col justify-between mt-8 h-[88vh]">
+        <div>
+          <h2
+            class="text-xl block mb-2 font-semibold text-gray-800 py-3 hover:bg-gray-200 rounded cursor-pointer"
+          >
+            Categories
+          </h2>
+          <div
+            v-for="(value, key) in categories"
+            :key="key"
+            @click="setFilterCategories(key)"
+            :class="value ? 'font-bold' : 'font-regular'"
+          >
+            {{ key }}
+          </div>
+          <h2
+            class="text-xl block mb-2 font-semibold text-gray-800 py-3 hover:bg-gray-200 rounded cursor-pointer"
+          >
+            Size
+          </h2>
+          <div
+            v-for="(valueSize, keySize) in size"
+            :key="keySize"
+            @click="setFilterSize(keySize)"
+            :class="valueSize ? 'font-bold' : 'font-regular'"
+          >
+            {{ keySize }}
+          </div>
+          <h2
+            class="text-xl block mb-2 font-semibold text-gray-800 py-3 hover:bg-gray-200 rounded cursor-pointer"
+          >
+            Color
+          </h2>
+          <div class="grid grid-cols-6">
+            <div v-for="(value, key) in color" :key="key">
+              <div
+                class="rounded-full border border-gray-200 w-4 h-4 m-1"
+                :style="{ backgroundColor: key }"
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <button
+            class="btn btn-invert"
+            @click="resetFilter, (isOpen = !isOpen)"
+          >
+            Reset Filter
+          </button>
+          <button class="btn btn-invert" @click="isOpen = !isOpen">
+            Save Filter
+          </button>
+        </div>
+      </div>
     </div>
   </transition>
 </template>
@@ -43,12 +78,40 @@
     data() {
       return {
         isOpen: false,
+        columns: null,
+        categories: this.$store.state.products.categories,
+        color: this.$store.state.products.color,
+        size: this.$store.state.products.size,
+        hej: false,
+        White: '#FFFFFF',
+        Gray: '#713f12',
+        Black: '#52525b',
+        Brown: '#27272a',
+        Green: '#000000',
+        Red: '#',
+        Yellow: '#',
+        Blue: '#',
+        filterCategories: this.$store.state.products.categories,
+        filterSize: this.$store.state.products.size,
+        filterColor: this.$store.state.products.color,
       }
     },
     props: {
       toggle: {
-        type: Boolean,
-        default: true,
+        required: true,
+        type: [Function, Boolean],
+      },
+    },
+    emits: ['reset'],
+    computed: {
+      products() {
+        return this.$store.state.products.productList
+      },
+      productCategories() {
+        return [...new Set(this.products.map(({ category }) => category))]
+      },
+      productSize() {
+        return [...new Set(this.products.map(({ size }) => size))]
       },
     },
     watch: {
@@ -68,6 +131,51 @@
       $route() {
         this.isOpen = false
       },
+      filtersCategories() {
+        this.setFilterCategories()
+      },
+      filtersSize() {
+        this.setFilterSize()
+      },
+    },
+
+    methods: {
+      testing() {
+        console.log(this.$store.state.products.categories)
+      },
+      setFilterCategories(x) {
+        this.$store.commit('products/setFilterCategories', x)
+      },
+      setFilterSize(x) {
+        this.$store.commit('products/setFilterSize', x)
+      },
+
+      resetFilter() {
+        this.$emit('reset')
+      },
+
+      /*  async getColumns() {
+        const response = await fetch(
+          'http://SITsApi.us-east-1.elasticbeanstalk.com/products/columns',
+        )
+        const result = await response.json()
+        this.columns = await result.data
+        for (let index = 0; index < this.columns.length; index++) {
+          const element = this.columns[index]
+          if (element.Field === 'category2') {
+            let test = element.Type
+            this.categories = test.slice(4, -1).replaceAll("'", '').split(',')
+          }
+          if (element.Field === 'color') {
+            let test = element.Type
+            this.color = test.slice(4, -1).replaceAll("'", '').split(',')
+          }
+          if (element.Field === 'size') {
+            let test = element.Type
+            this.size = test.slice(4, -1).replaceAll("'", '').split(',')
+          }
+        }
+      },*/
     },
   }
 </script>
--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -7,19 +7,93 @@
       ProductCard,
       SortFilter,
     },
-    computed: {
-      products() {
-        return this.$store.state.products.productList
-      },
-    },
     data() {
       return {
         closeMenu: true,
+        productList: this.$store.state.products.productList,
+        filterCategories: Object.entries(this.$store.state.products.categories),
+        /* filteredCategories: null,*/
+        filterSize: this.$store.state.products.size,
+        filterColor: this.$store.state.products.color,
+        filteredProducts: [],
+        allObjects: [
+          ...Object.entries(this.$store.state.products.categories),
+          ...Object.entries(this.$store.state.products.size),
+          ...Object.entries(this.$store.state.products.color),
+        ],
+        filteredObjects: null,
       }
     },
+    watch: {
+      allFalse() {
+        this.showAll()
+      },
+    },
     methods: {
+      resetProducts() {
+        this.filteredProducts = this.productList
+        this.allObjects.forEach((x) => (x[1] = false))
+        console.log(this.filteredProducts)
+        console.log(this.allObjects)
+        console.log('it probobly works')
+      },
+      showAll() {
+        console.log(this.allObjects)
+        this.allObjects.forEach((x) => console.log(x[1]))
+
+        if (this.allObjects.every((x) => x[1] === false)) {
+          this.filteredProducts = this.productList
+          console.log(this.filteredProducts)
+          console.log('if true')
+        } else {
+          console.log('if false')
+        }
+      },
+
+      testing() {
+        for (const [key, value] of Object.entries(this.filterCategories)) {
+          const newarray = Object.entries(this.filterCategories).filter(
+            (o) => o[1] === true,
+          )
+
+          // filter 1
+          console.log(`${key}: ${value}`)
+          if (value) {
+            // filter 1
+            for (let index = 0; index < this.productList.length; index++) {
+              // filter 2
+              const element = this.productList[index]
+              if (element.category2 === key) {
+                console.log(element.title, 'woopi')
+                this.filteredProducts.push(element) // [].concat(arr1, arr2, ...)
+              }
+            }
+          }
+        }
+      },
       toggleMenu() {
         this.closeMenu = !this.closeMenu
+      },
+      showFiltered() {
+        console.log(this.$store.state.products.categories)
+        for (const [key, value] of Object.entries(
+          this.$store.state.products.categories,
+        )) {
+          // filter 1
+          console.log(`${key}: ${value}`)
+          if (value) {
+            // filter 1
+            for (let index = 0; index < this.productList.length; index++) {
+              // filter 2
+              const element = this.productList[index]
+              if (element.category2 === key) {
+                console.log(element.title, 'woopi')
+                this.filteredProducts.push(element) // [].concat(arr1, arr2, ...)
+              }
+            }
+          }
+        }
+        console.log(this.filteredProducts)
       },
     },
   }
@@ -27,13 +101,15 @@
 
 <template>
   <main>
-    <SortFilter :toggle="this.closeMenu" />
+    <button class="btn btn-invert" @click="showAll">Testar</button>
+    <button class="btn btn-invert" @click="showFiltered">showFiltered</button>
+    <SortFilter :toggle="this.closeMenu" @reset="resetProducts" />
     <div
       class="flex flex-row flex-wrap gap-x-16 gap-y-8 justify-center my-8"
       @click="toggleMenu"
     >
       <ProductCard
-        v-for="product in products"
+        v-for="product in filteredProducts"
         :product="product"
         :key="product.id"
         :max-width="'max-w-[90%] sm:max-w-[45%] lg:max-w-[30%] '"
