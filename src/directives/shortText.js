1e83afbc2937cab2d4f1c20d847711a7aa81696d
Marcus Oskarsson <MarcusOskarsson@hotmail.com>
2023-03-15 15:09:59 +0100

    fix shorttext

--- src/directives/shortText.js
+++ src/directives/shortText.js
@@ -2,5 +2,6 @@ export default function (element, binding) {
   console.log(element, binding)
   const text = binding.value.text
   const maxChars = binding.value.chars || 30
-  element.innerText = text.length >= 30 ? text.slice(0, maxChars) + '...' : text
+  element.innerText =
+    text.length >= maxChars ? text.slice(0, maxChars) + '...' : text
 }
